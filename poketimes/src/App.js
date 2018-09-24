8ea3e3bfd1078ef6cb30860427c85f62b5f863ef
Michael <mikeishere3@gmail.com>
2018-09-24 13:13:12 -0400

    added posts to home page, added css for post styling

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Post from './components/Post'
 
 class App extends Component {
   render() {
@@ -11,10 +12,13 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home} />
-          <Route path='/About' component={About} />
-          <Route path='/Contact' component={Contact} />
-        </div>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/About' component={About} />
+            <Route path='/Contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+          </Switch>
+          </div>
       </BrowserRouter>
     );
   }
