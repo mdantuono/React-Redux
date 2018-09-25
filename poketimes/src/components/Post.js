dcbe5051a52e6556bda94670ddec44659ee41f59
Michael <mikeishere3@gmail.com>
2018-09-25 12:23:48 -0400

    delete post added, mapped dispatch to props

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Post extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/');
+  }
   render() {
     const post = this.props.post ? (
       <div className="posts">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>Delete Post</button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -27,4 +34,10 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(Post)
+const mapDispatchTopProps = (dispatch) => {
+  return {
+    deletePost: (id) => { dispatch({ type: 'DELETE_POST', id: id }) }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchTopProps)(Post)
